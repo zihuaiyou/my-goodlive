af66b04852d354dfae51e7fe4b4cecfebbff2f64
zihuaiyou <976497972@qq.com>
2022-05-10 21:42:19 +0800

    update0510

---
+++ src/pages/Search/SearchHeader/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.less'
+import SearchInput from '../../../components/SearchInput'
+
+const SearchHeader = () => {
+    const handleBack = () => {
+        window.history.back();
+    }
+    return (
+        <div id='search-header' className='clear-fix'>
+            <span className='back-icon float-left' onClick={handleBack}>
+                <i className='icon-chevron-left'></i>
+            </span>
+            <div className='input-container'>
+                <i className='icon-search'></i>
+                <SearchInput/>
+            </div>
+        </div>
+    )
+}
+export default SearchHeader
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import SearchDataList from "./SearchDataList";
+import SearchHeader from './SearchHeader'
 
 const Search = () => {
   // useParams:获取路由传递的参数
   const params = useParams();
   return (
     <div>
+      <SearchHeader/>
       <SearchDataList keywords = {params.keywords}/>
     </div>
   );
